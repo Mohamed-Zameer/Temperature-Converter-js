f94438892f032b88451528e15214b3280bd7f0e3
Mohamed Zameer <143885585+Mohamed-Zameer@users.noreply.github.com>
2024-03-23 15:44:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const textbox = document.getElementById("textbox");
 const tofahrenheit = document.getElementById("tofahrenheit");
-const tocelcius = document.getElementById("tocelcius");
+const tocelsius = document.getElementById("tocelsius");
 const result = document.getElementById("result");
 const reset = document.getElementById("reset");
 let temp;
@@ -15,7 +15,7 @@ function convert(){
     temp = temp * 9/5 + 32;
     result.textContent = temp.toFixed(1) + "°F";
   }
-  else if(tocelcius.checked){
+  else if(tocelsius.checked){
     temp = Number(textbox.value);
     temp = (temp - 32) * (5/9);
     result.textContent = temp.toFixed(1) + "°C";
